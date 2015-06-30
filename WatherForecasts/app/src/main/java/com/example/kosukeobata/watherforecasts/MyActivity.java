1def23014c0dd9d024c39d88058bf3d060b2fd0f
kosukeobata <kosuke.obata@bizreach.co.jp>
2015-06-30 13:50:59 +0900

    追加

--- WatherForecasts/app/src/main/java/com/example/kosukeobata/watherforecasts/MyActivity.java
+++ WatherForecasts/app/src/main/java/com/example/kosukeobata/watherforecasts/MyActivity.java
@@ -1,84 +1,60 @@
 package com.example.kosukeobata.watherforecasts;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.widget.Toast;
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
 
-public class MyActivity extends Activity {
-    private TextView location;
-    private LinearLayout forecastLayout;
-    private ProgressBar progress;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MyActivity extends ActionBarActivity {
+
+    private static final String[] POINT_LIST = {
+            "270000",
+            "130010",
+            "040010"};
+
+    private List<String> pointList;
+
+    private Adapter adapter;
+    private ViewPager viewPager;
+
+    private class Adapter extends FragmentStatePagerAdapter {
+
+        public Adapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return FragmentWeather.newInstance(pointList.get(position));
+        }
+
+        @Override
+        public int getCount() {
+            return pointList.size();
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
 
-        location = (TextView) findViewById(R.id.tv_location);
-        forecastLayout = (LinearLayout) findViewById(R.id.ll_forecasts);
-        progress = (ProgressBar) findViewById(R.id.progress);
+        if (pointList == null) {
+            pointList = Arrays.asList(POINT_LIST);
 
-        new GetWeatherForecastTask(this).execute("400040");
+        }
+
+        viewPager = (ViewPager) findViewById(R.id.vp_main);
+        adapter = new Adapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
     }
-
-    private class GetWeatherForecastTask extends GetWeatherForecastApiTask {
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            progress.setVisibility(View.VISIBLE);
-        }
-
-        public GetWeatherForecastTask(Context context) {
-            super(context);
-        }
-
-        @Override
-        protected void onPostExecute(WeatherForecast data) {
-            super.onPostExecute(data);
-
-            progress.setVisibility(View.GONE);
-
-            if (data != null) {
-                location.setText(data.location.area + " " +
-                        data.location.prefecture + " " + data.location.city);
-
-                //予報を一覧表示
-                for (WeatherForecast.Forecast forecast : data.forecastList) {
-                    View row = View.inflate(MyActivity.this, R.layout.forecasts_row, null);
-
-                    TextView date = (TextView) row.findViewById(R.id.tv_date);
-                    date.setText(forecast.dateLabel);
-
-                    TextView telop = (TextView) row.findViewById(R.id.tv_telop);
-                    telop.setText(forecast.telop);
-
-                    TextView temp = (TextView) row.findViewById(R.id.tv_tempreture);
-                    temp.setText(forecast.temperature.toString());
-
-                    ImageView imageView = (ImageView) row.findViewById(R.id.iv_weather);
-                    imageView.setTag(forecast.image.url); //読み込むURを設定
-
-                    //読み込み処理の実行
-                    ImageLoaderTask task = new ImageLoaderTask(MyActivity.this);
-                    task.execute(imageView);
-
-                    forecastLayout.addView(row);
-                }
-            } else if (exception != null) {
-                Toast.makeText(MyActivity.this, exception.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-
-
+}
 
 //    @Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
@@ -101,4 +77,3 @@ public class MyActivity extends Activity {
 //
 //        return super.onOptionsItemSelected(item);
 //    }
-}
